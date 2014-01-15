c782c9e4fc71c57da12b4fa837b8d0bbaa25c962
root <root@mx.byte.nl>
2014-01-15 16:00:37 +0100

    added defaults

--- setup-dev-server.sh
+++ setup-dev-server.sh
@@ -1,11 +1,60 @@
 #!/bin/bash  
 
+DEFAULT_DIR=`pwd`"/defaults"
 
-echo -n "---> checking if this is a Debian installation ... "
-if [ -f /etc/debian_version ]; then
-    echo "yes. Invoking ./setup-dev-debian.sh"
-    ./setup-dev-debian.sh
-    exit 0
-else
-    echo "no"
-fi
+
+
+install_default_configs () {
+    cp ${DEFAULT_DIR}/vimrc ~/.vimrc
+}
+
+
+
+
+
+pre_setup_steps () {
+    echo "---> STEP: pre-setup steps"
+
+    install_default_configs
+
+    echo "---> DONE"
+    echo
+}
+
+
+
+
+
+distro_specific_setup () {
+    echo "---> STEP: starting distro specific install"
+    echo -n "checking if this is a Debian installation ... "
+    DISTRO="unknown"
+
+    if [ -f /etc/debian_version ]; then
+        echo "yes."
+        DISTRO="debian"
+    else
+        echo "no"
+    fi
+
+    case "${DISTRO}" in
+        debian       ) ./setup-dev-debian.sh;;
+        *            ) echo "unknown distro!";;
+    esac
+}
+
+
+
+post_setup_steps () {
+    echo "---> STEP: post-setup steps"
+
+    echo "---> DONE"
+    echo
+}
+
+
+### MAIN
+
+pre_setup_steps
+distro_specific_setup
+post_setup_steps
